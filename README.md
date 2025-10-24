93dd21ab8a1aaab39b75d9ba92083195cf28c66b
SeaThitipat <9thitipat@email.com>
2025-10-24 19:45:56 +0700

    Refactor README.md for clarity; update index.html structure; enhance goodies_list.html layout; improve product card hover effect in main.js

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-This repository is use for give an assignment to YPD2025 activity!
-Please don't mock me nor comment any issue i post here (except you are YPD2025 mentor)!
-
-thank you for reading!
+Tech stack ที่ใช้
+- vanilla (html,css,javascript) -->
+- tailwind --> Easy and Efficient for web develop
--- main.js
+++ main.js
@@ -17,15 +17,15 @@ async function fetchProducts() {
     products.forEach(product => {
       const card = document.createElement("div");
       card.id = `product-${product.id}`;
-      card.className = "w-2/3 bg-sky-100/70 rounded-2xl shadow-2xl snap-start transition duration-300 hover:ease-(--move-right) hover:shadow-cyan-50/50 hover:bg-sky-100 hover:w-full";
+      card.className = "bg-sky-100/70 rounded-2xl shadow-2xl snap-start transition ease-in-out hover:scale-105 hover:shadow-cyan-50/50 hover:bg-sky-100 ";
       card.innerHTML = `
-        <img src="${product.image}" alt="${product.title}" class="w-full h-[200px] object-contain mb-3"/>
-        <h2 class="text-lg font-semibold line-clamp-2">${product.title}</h2>
-        <div class="flex justify-between items-center mt-2">
+        <img src="${product.image}" alt="${product.title}" class=" w-full h-[200px] object-contain mb-3"/>
+        <h2 class="ml-[5px] text-lg font-semibold line-clamp-2">${product.title}</h2>
+        <div class="ml-[5px] flex justify-between items-center mt-2">
           <span class="text-blue-600 font-bold ">$${product.price}</span>
           <span class="text-sm text-gray-500">${product.category}</span>
         </div>
-        <button class="w-full mt-4 bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">Add to Cart
+        <button class="w-full mt-2 content-end bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 transition">Add to Cart
         </button>
       `;
       productList.appendChild(card);
